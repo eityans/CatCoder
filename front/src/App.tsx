7d6868b15817709457b9fa2a71939ebfbbaea8f3
eityans <eityans@gmail.com>
2021-03-07 22:55:49 +0900

    useEffect

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+
+  const [text, setText] = useState<String | null>(null);
+
+  useEffect(() => {
+    const fetchInit = {
+      method: "GET",
+      headers: { "content-type": "application/json" }
+    };
+
+    const parsedURL = new URL("hello_world", process.env.REACT_APP_SERVER_URL).href;
+    fetch(parsedURL, fetchInit)
+      .then(response => response.json())
+      .then(response => setText(response.text));
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Response: {text}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +34,7 @@ function App() {
       </header>
     </div>
   );
+
 }
 
 export default App;
