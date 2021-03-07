6c7006bf7efad234424b394ddf496a2267fe15d9
eityans <eityans@gmail.com>
2021-03-07 16:12:20 +0900

    add endpoint for prod

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /localhost\:\d+/
+    origins /localhost\:\d+/, 'https://catcoder.herokuapp.com/api'
 
     resource '*',
       headers: :any,
